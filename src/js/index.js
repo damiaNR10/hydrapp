c32bd477b277f6690dea01a3ce7b1ccfb749b785
damiaNR10 <damianrog96@gmail.com>
2020-06-29 00:12:58 +0200

    main part

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,36 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+let addGlass = document.querySelector('.main-section__button--js-add');
+let deleteGlass = document.querySelector('.main-section__button--js-delete');
+let counter = document.querySelector('.main-section__counter--js');
+
+const key = new Date().toISOString().slice(0, 10);
+let value;
+
+if(!localStorage.getItem('key')) {
+    localStorage.setItem('key', 0);
+}
+
+addGlass.addEventListener('click', () => {
+    value = parseInt(localStorage.getItem('key'));
+    localStorage.setItem('key', ++value);
+    counter.innerHTML = localStorage.getItem('key');
+});
+
+deleteGlass.addEventListener('click', () => {
+    if(localStorage.getItem('key') > 0){
+        value = parseInt(localStorage.getItem('key'));
+        localStorage.setItem('key', --value);
+        counter.innerHTML = localStorage.getItem('key');
+    }
+});
+
+counter.innerHTML = localStorage.getItem('key');
+
+//localStorage.removeItem('key');
--- webpack.dev.js
+++ webpack.dev.js
@@ -41,7 +41,7 @@ module.exports = merge(common, {
   },
 
   devServer: {
-    port: 8080,
+    port: 8081,
     host: '0.0.0.0'
   },
 });
